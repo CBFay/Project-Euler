ec174697708fa0621775ff1f1669787e6fe6b303
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-12-30 23:17:15 -0500

    Create euler_0002.py

---
+++ Euler 0002 - Even Fibonacci Numbers/euler_0002.py
@@ -0,0 +1,14 @@
+# https://projecteuler.net/problem=2
+# created 12.30.2017 by CB Fay
+
+s = 0
+a = 1
+b = 2
+
+while b < 4000000:
+    if b % 2 == 0:
+        s += b
+        
+    a, b = b, a+b    
+
+print(s)
